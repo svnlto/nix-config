396b03ea92d28d27beb06915e0e016c421038dd2
Sven Lito <me@svenlito.com>
2025-10-30 10:19:07 +0700

    fix(neovim): use idiomatic LazyVim patterns for treesitter and lualine
    - Use opts function with vim.list_extend for treesitter parsers (LazyVim best practice)
    - Register treesitter languages in opts function instead of custom config
    - Map yaml parser to yaml.jinja compound filetype for proper highlighting
    - Add lualine config to explicitly show filetype in statusline
    - Remove language registration from init.lua (now in treesitter plugin)

--- common/neovim/lua/plugins/treesitter.lua
+++ common/neovim/lua/plugins/treesitter.lua
@@ -2,41 +2,17 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"typescript",
-				"tsx",
-				"json",
-				"go",
-				"gomod",
-				"gowork",
-				"terraform",
-				"yaml",
-				"markdown",
-				"dockerfile",
+		opts = function(_, opts)
+			-- Extend ensure_installed list (LazyVim uses opts_extend for this)
+			vim.list_extend(opts.ensure_installed, {
 				"jinja2",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+			})
 
-			-- Register jinja2 parser for compound filetypes
+			-- Register language mappings for compound filetypes
+			vim.treesitter.language.register("yaml", "yaml.jinja")
 			vim.treesitter.language.register("jinja2", "jinja")
+
+			return opts
 		end,
 	},
 }
