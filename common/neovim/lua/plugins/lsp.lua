e290a5804d25807b27c47a3cb7c063579e5d0a2e
Sven Lito <me@svenlito.com>
2025-10-30 10:59:20 +0700

    fix(neovim): disable helm_ls internal yamlls diagnostics for Jinja2 templates
    - helm_ls spawns yamlls as internal subprocess for YAML validation
    - This causes "Yamlls:" prefixed errors on Jinja2 cookiecutter templates
    - Set diagnosticsLimit = 0 to disable yamlls diagnostics entirely
    - Preserves hover and completion features from yamlls
    - Solves triple-layer templating issues (Jinja2 + Helm + YAML)

--- common/neovim/lua/plugins/lsp.lua
+++ common/neovim/lua/plugins/lsp.lua
@@ -7,18 +7,15 @@ return {
 			inlay_hints = { enabled = false }, -- Disable inlay hints by default (toggle with <leader>uh)
 			-- Configure LSP servers
 			servers = {
-				-- YAML Language Server - disable for helm templates (which may contain Jinja2)
-				yamlls = {
-					filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
-					-- Helm templates are handled by helm_ls, not yamlls
-					root_dir = function(fname)
-						local lspconfig = require("lspconfig")
-						-- Don't attach yamlls to helm chart templates
-						if fname:match("/templates/.*%.yaml$") or fname:match("/templates/.*%.yml$") then
-							return nil
-						end
-						return lspconfig.util.root_pattern(".git")(fname)
-					end,
+				-- Helm Language Server - disable yamlls diagnostics for Jinja2 templates
+				helm_ls = {
+					settings = {
+						["helm-ls"] = {
+							yamlls = {
+								diagnosticsLimit = 0, -- Disable yamlls diagnostics (keeps hover/completion)
+							},
+						},
+					},
 				},
 				-- TypeScript/JavaScript (updated name)
 				ts_ls = {
